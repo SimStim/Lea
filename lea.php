240e5dd1f7bd3038499adaa313fd402bcb5470df
Eduard Pech <villa.straylight.2020+github@gmail.com>
2026-04-21 05:21:40 -0300

    Skip Slush Pile in Heath mode.

--- lea.php
+++ lea.php
@@ -22,9 +22,14 @@ if (Girlfriend::comeToMe()->recall(name: "heath-mode") === "yes") {
     foreach ($heath->ebookFiles as $fileName) {
         echo $heath->heathName . Fancy::BG_BLUE . " $fileName " . Fancy::RESET . PHP_EOL;
         Girlfriend::comeToMe()->myNameIsLea();
+        if ($fileName === "slush.xml") {
+            echo "... skipping!" . PHP_EOL;
+            continue;
+        }
         $work = new PaisleyPark(fileName: $fileName);
         $work->pControl();
         $heath->makeIndex($work)->writeIndex();
+        $work = null;
     }
 } else {
     $work = new PaisleyPark(fileName: $argv[1] ?? "");
